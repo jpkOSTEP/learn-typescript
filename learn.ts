461b87d74feb92aa95b5d92e0678de21d8cdbc85
Jay Kania <120383316+jpkOSTEP@users.noreply.github.com>
2025-05-01 19:38:18 -0400

    feat(utils): add delay function

--- learn.ts
+++ learn.ts
@@ -1,58 +1,5 @@
 
 // Appended by cron on 2025-05-01
-
-
-// Appended by cron on 2025-05-01
-$ npm start
-
-And call it:
-
-$ npm run
-
-It should then compile and run with the following error message:
-
-Could not load ':api/json' on port 3000
-
-The solution to this problem is to create a function that only accepts JSON and returns a Promise as an argument.
-
-This should be straightforward:
-
-$ npm run
-
-then, you can call it to fetch JSON from a given URL:
-
-$ npm run
-
-This should return:
-
-Request: json
-
-If you need to wait for something, you can use the --wait-for option.
-
-Example:
-
-$ npm run fetchJSON
-
-And then try to execute it
-
-// Appended by cron on 2025-05-01
-export function getJSON(url: string): Promise<any> { const res = await fetchJSON(url); return res.json(); }
-
-- export async function fetchJSON(url: string): Promise<any> { const res = await fetchJSON(url); return res.json(); }
-
-
-export function getJSON(url: string): Promise<any> { const res = await fetchJSON(url); return res.json(); }
-
-You'll see that there are some extra functions that you can use to get and set JSON data. But you should use them in a way that is convenient to the caller.
-
-You don't have to have this much
-
-// Appended by cron on 2025-05-01
-export function noop(): void {
-  console.log("No operation performed.");
-}
-
-// Appended by cron on 2025-05-01
 export function noop(): void {
   console.log("No operation performed.");
 }
