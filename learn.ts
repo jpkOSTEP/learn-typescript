25fca7b32ac803da42762b0c97ba31e5a6a0b3f3
Jay Kania <120383316+jpkOSTEP@users.noreply.github.com>
2025-05-01 22:05:10 -0400

    feat(ts): append daily snippet (2025-05-01)

--- learn.ts
+++ learn.ts
@@ -152,3 +152,27 @@ function isPalindrome(input: string): boolean {
 function filterArrayByType<T>(array: unknown[], type: string): T[] {
   return array.filter(item => typeof item === type) as T[];
 }
+
+class Stack<T> {
+  private items: T[] = [];
+
+  push(item: T): void {
+    this.items.push(item);
+  }
+
+  pop(): T | undefined {
+    return this.items.pop();
+  }
+
+  peek(): T | undefined {
+    return this.items[this.items.length - 1];
+  }
+
+  isEmpty(): boolean {
+    return this.items.length === 0;
+  }
+
+  size(): number {
+    return this.items.length;
+  }
+}
