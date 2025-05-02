6aebe51077b23ce422f6a16d822ef9d54437cf32
Jay Kania <120383316+jpkOSTEP@users.noreply.github.com>
2025-05-01 20:30:28 -0400

    feat(ts): append daily snippet (2025-05-01)

--- learn.ts
+++ learn.ts
@@ -3,3 +3,8 @@ function getUniqueElements<T>(array: T[]): T[] {
     const uniqueElements = new Set<T>(array);
     return Array.from(uniqueElements);
 }
+
+function findMax<T extends number | string>(items: T[]): T | null {
+  if (items.length === 0) return null;
+  return items.reduce((max, item) => (item > max ? item : max));
+}
