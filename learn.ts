885c028c59630946673e38c07192df722ca80ef1
Jay Kania <120383316+jpkOSTEP@users.noreply.github.com>
2025-05-01 20:26:12 -0400

    feat(ts): append daily snippet (2025-05-01)

--- learn.ts
+++ learn.ts
@@ -1,7 +1,14 @@
 
-// Appended by cron on 2025-05-01
 ```typescript
-function getUniqueElements<T>(array: T[]): T[] {
-  return Array.from(new Set(array));
+function flattenArray<T>(nestedArray: (T | T[])[]): T[] {
+  const result: T[] = [];
+  nestedArray.forEach(item => {
+    if (Array.isArray(item)) {
+      result.push(...flattenArray(item));
+    } else {
+      result.push(item);
+    }
+  });
+  return result;
 }
 ```
